b939c3461bcdbf0d86ad6fc8fa274ca1ca0598bc
santiagoalberto416 <santiagoalberto416@gmail.com>
2017-01-22 02:30:21 -0800

    login changes

--- app/src/main/java/com/skirk/smartdisplay/MainActivity.java
+++ app/src/main/java/com/skirk/smartdisplay/MainActivity.java
@@ -1,8 +1,12 @@
 package com.skirk.smartdisplay;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,6 +19,11 @@ public class MainActivity extends AppCompatActivity{
     TextView latitude;
     TextView longittude;
     Location location;
+
+    private String firstName = "";
+    private String lastName = "";
+    private String imageFile = "";
+    private int idUser = 0;
 
     // The minimum distance to change Updates in meters
     private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 0; // 10 meters
@@ -32,14 +41,17 @@ public class MainActivity extends AppCompatActivity{
             float distanceInMeters = location.distanceTo(MainActivity.this.location);
             Log.d("Distance", distanceInMeters+" ");
 
-            Location location1=new Location("locationA");
-            location1.setLatitude(17.372102);
-            location1.setLongitude(78.484196);
-            Location location2=new Location("locationA");
-            location2.setLatitude(17.375775);
-            location2.setLongitude(78.469218);
-            double distance=location2.distanceTo(location1);
-            Log.d("testDistance", distance+" ");
+            if(distanceInMeters<10){
+                Log.d("notification", "Enter into notification");
+                Notification n  = new Notification.Builder(MainActivity.this)
+                        .setContentTitle("New mail from " + "test@gmail.com")
+                        .setContentText("Subject")
+                        .setSmallIcon(R.drawable.new_business)
+                        .setAutoCancel(true).build();
+                NotificationManager notificationManager =
+                        (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                notificationManager.notify(0, n);
+            }
         }
         @Override
         public void onStatusChanged(String provider, int status, Bundle extras) {
@@ -68,10 +80,18 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // get user from previous activity
+        idUser = getIntent().getIntExtra("id", 0);
+        imageFile = getIntent().getStringExtra("image");
+        firstName = getIntent().getStringExtra("first");
+        lastName = getIntent().getStringExtra("last");
+
+
         location = new Location("");
         //32.52239863, -117.01884061
-        location.setLatitude(32.52239863);
-        location.setLongitude(-117.01884061);
+        location.setLatitude(32.462516);
+        location.setLongitude(-116.821192);
 
         latitude = (TextView)this.findViewById(R.id.lattitude);
         longittude = (TextView)this.findViewById(R.id.longitude);
